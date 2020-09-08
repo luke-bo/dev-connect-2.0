26a98dd53e9f4271786272c9e4cf38cd52b9441c
luke.bohonis <w.bohonis@gmail.com>
2020-09-08 13:50:50 -0700

    Add jwt to register user route.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } =  require('express-validator');
 
 // Bring in User model
@@ -63,13 +65,25 @@ async (req, res) => {
     user.password = await bcrypt.hash(password, salt);
     // Step #3: Save user to database
     await user.save();
+    // Step#4: Return json webtoken
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
 
-    // Return json webtoken
-    res.send('User route')
-
+    jwt.sign(
+      payload, 
+      config.get('jwtSecret'),
+      { expiresIn: 36000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
   } catch(err) {
     console.error(err.message);
-    response.status(500).send('Server error');
+    res.status(500).send('Server error');
   };
 });
 
