f52cad9003b18a5ee0fe9042913eabc1e4dc44d1
luke.bohonis <w.bohonis@gmail.com>
2020-09-09 12:45:39 -0700

    Add jwt auth middleware.

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+// export our middleware function that has access to the req, res cycle
+module.exports = function(req, res, next) {
+  // Get token from header
+  const token = require.header('x-auth-token');
+
+  // Check if no token
+  if(!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied.' });
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    require.user = decoded.user;
+    next();
+  } catch(err) {
+     res.status(401).json({ msg: 'Token is not valid' });
+  }
+}
