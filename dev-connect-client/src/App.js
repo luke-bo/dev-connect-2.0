5c8cd3c6d480bbf66145a379600725f750ff6d54
luke.bohonis <w.bohonis@gmail.com>
2020-09-30 23:19:40 -0700

    Refactor standard create-react-app structure and add basic navbar and landing components.

--- dev-connect-client/src/App.js
+++ dev-connect-client/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { NavBar } from './components/layouts/NavBar';
+import { Landing } from './components/layouts/Landing';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Fragment>
+    <NavBar></NavBar>
+    <Landing></Landing>
+  </Fragment>
+);
 
 export default App;
+
+
+
+// <header className="App-header">
+// <img src={logo} className="App-logo" alt="logo" />
+// <p>
+//   Edit <code>src/App.js</code> and save to reload.
+// </p>
+// <a
+//   className="App-link"
+//   href="https://reactjs.org"
+//   target="_blank"
+//   rel="noopener noreferrer"
+// >
+//   Learn React
+// </a>
+// </header>
