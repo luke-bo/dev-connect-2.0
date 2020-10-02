2323380cdfb7a92762cb51eaa609a464ade721fc
luke.bohonis <w.bohonis@gmail.com>
2020-10-01 21:51:17 -0700

    Config react-router-dom. Create Login and Register component structure. Refactor NavBar and Landing components to use Link.

--- dev-connect-client/src/App.js
+++ dev-connect-client/src/App.js
@@ -1,13 +1,24 @@
 import React, { Fragment } from 'react';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { NavBar } from './components/layouts/NavBar';
 import { Landing } from './components/layouts/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <NavBar></NavBar>
-    <Landing></Landing>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <NavBar />
+      <Route exact path='/' component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
