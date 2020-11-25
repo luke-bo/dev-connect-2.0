4a21a52c4f64fdc9665feaa7abfb43a63abb9aa4
luke.bohonis <w.bohonis@gmail.com>
2020-11-24 16:28:02 -0800

    Add auth redux state management to our register component. Successfully tested creating new user in db.

--- dev-connect-client/src/actions/auth.js
+++ dev-connect-client/src/actions/auth.js
@@ -3,33 +3,33 @@ import { setAlert } from "./alert";
 import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
 
 // Register User
-export const register = ({ name, email, password }) => async dispatch => {
+export const register = ({ name, email, password }) => async (dispatch) => {
   const config = {
     headers: {
-      'Content-Type': 'application/json'
-    }
-  }
+      "Content-Type": "application/json",
+    },
+  };
 
   const body = JSON.stringify({ name, email, password });
 
   try {
-    const res = await axios.post('/api/users', body, config);
+    const res = await axios.post("/api/users", body, config);
 
     dispatch({
       type: REGISTER_SUCCESS,
-      payload: res.data
+      payload: res.data,
     });
   } catch (err) {
     const errors = err.response.data.errors;
 
-    if(errors) {
-      errors.forEach(error => {
-        dispatch(setAlert(error.msg, 'danger'))
+    if (errors) {
+      errors.forEach((error) => {
+        dispatch(setAlert(error.msg, "danger"));
       });
     }
 
     dispatch({
-      type: REGISTER_FAIL
+      type: REGISTER_FAIL,
     });
   }
-}
+};
