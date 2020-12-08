1352afa379bf64d57499c4914a5ada295f4298e0
luke.bohonis <w.bohonis@gmail.com>
2020-12-08 14:54:49 -0800

    Add remainder of redux logic to NavBar component. Map auth state to NavBar to props so NavBar can display login/register links or logout link based on isAuthenticated value in state.

--- dev-connect-client/src/App.js
+++ dev-connect-client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { NavBar } from "./components/layouts/NavBar";
+import NavBar from "./components/layouts/NavBar";
 import { Landing } from "./components/layouts/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
--- dev-connect-client/src/components/layouts/NavBar.js
+++ dev-connect-client/src/components/layouts/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
@@ -8,9 +8,9 @@ const NavBar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
-        <a onClick={logout} href="!*">
+        <a onClick={logout} href="*!">
           <i className="fas fa-sign-out-alt"></i>{' '}
-          Logout
+          <span className="hide-sm">Logout</span>
         </a>
       </li>
     </ul>
@@ -19,7 +19,7 @@ const NavBar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <a href="!*">Developers</a>
+        <a href="*!">Developers</a>
       </li>
       <li>
         <Link to="/register">Register</Link>
@@ -37,12 +37,12 @@ const NavBar = ({ auth: { isAuthenticated, loading }, logout }) => {
           <i className="fas fa-code"></i> DevConnector
         </Link>
       </h1>
-  
+      { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>)}
     </nav>
   );
 };
 
-NavBar.PropTypes = {
+NavBar.propTypes = {
   logout: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
 };
