ef5d497cdd573e4a3ee0e2e5e3e85cb56c705aab
luke.bohonis <w.bohonis@gmail.com>
2021-01-04 10:53:11 -0800

    Add clear_profile action type and handling in profile reducer. This handles clearing a user's profile and repos on logout. Add markup to Dashboard to prompt users to create a new profile if one does not exist.

--- dev-connect-client/src/reducers/profile.js
+++ dev-connect-client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+import { GET_PROFILE, CLEAR_PROFILE, PROFILE_ERROR } from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -16,6 +16,13 @@ export default function (state = initialState, action) {
       return {
         ...state,
         profile: payload,
+        loading: false,
+      };
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
         loading: false,
       };
     case PROFILE_ERROR:
